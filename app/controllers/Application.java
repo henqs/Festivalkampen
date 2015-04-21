5ec32b06505d5576dfcf4d86c6b54c2c0b608641
henqs <henq.sv@gmail.com>
2015-04-21 13:33:57 +0200

    Revert "Plan för Sprint 1.01"
    This reverts commit 3e2875482e80f53fdf82e7daa146842a5743ae47.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,28 +4,10 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
-import java.lang.Exception;
-import java.sql.*;
-import java.sql.DriverManager;
-import views.html.*;
-
 
 public class Application extends Controller {
-	
-	
 
     public static Result index() {
-		Connection conn = null;
-		Statement stmt = null;
-		String result = "";
-		
-		 try {   
-			 conn = DriverManager.getConnection
-			("jdbc:mysql://mysql.dsv.su.se/servernamn",
-					"Namn", "Lösenord");
-
-
-     
         return ok(index.render("Hej"));
     }
     
