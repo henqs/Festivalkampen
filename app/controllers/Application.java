f1b53be0678e4b4ecc1fbd35e1b039e4849947f3
henqs <henq.sv@gmail.com>
2015-04-22 13:17:51 +0200

    Revert "Henrik 0.02"
    This reverts commit 729c2445eb117d1d924aae1b0599aaa41402a9a9.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,42 +1,14 @@
 package controllers;
 
-import java.sql.*;
-
 import play.*;
-import play.libs.Json;
-import play.api.db.*;
 import play.mvc.*;
+
 import views.html.*;
 
 public class Application extends Controller {
-	
-	 Connection conn = null;
-	 Statement statement = null;
-	 ResultSet result = null;
 
     public static Result index() {
-    	
-    	 try {
-    		 conn = DB.getConnection();
-    		 stmt = conn.createStatement();
-    
-//    		 String insertIntoDatabase = "INSERT INTO user"
-//    				 + "(username, password) "
-//    				 + "hej"+ "dåligtlösneord";
-//    		 stmt.executeUpdate(insertIntoDatabase);
-    		 
-    		 statement = conn.createStatement();
-    	
-    	 String sql = "SELECT * FROM user";
-    	 return ok(index.render(sql.toString()));
-
-    }catch(Exception e){
-
-      return ok(index.render(e.toString()));
-
-   }
-   
-  //      return ok(index.render("Hej"));
+        return ok(index.render("Hej"));
     }
     
     public static void testPush() {
