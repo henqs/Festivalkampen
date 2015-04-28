43dd5883977403ea9de742d55bbb3995eefca811
henqs <henq.sv@gmail.com>
2015-04-28 15:58:31 +0200

    Något+1

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -7,10 +7,10 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			'global':	{ range: '*', href: 'css/style.css' },
-			'desktop':	{ range: '737-', href: 'css/style-desktop.css', containers: 1200, grid: { gutters: 40 } },
-			'1000px':	{ range: '737-1200', href: 'css/style-1000px.css', containers: 960, grid: { gutters: 25 }, viewport: { width: 1080 } },
-			'mobile':	{ range: '-736', href: 'css/style-mobile.css', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
+			'global':	{ range: '*', href: '@routes.Assets.at("stylesheets/css/style.css")' },
+			'desktop':	{ range: '737-', href: '@routes.Assets.at("stylesheets/css/style-desktop.css")', containers: 1200, grid: { gutters: 40 } },
+			'1000px':	{ range: '737-1200', href: '@routes.Assets.at("stylesheets/css/style-1000px.css")', containers: 960, grid: { gutters: 25 }, viewport: { width: 1080 } },
+			'mobile':	{ range: '-736', href: '@routes.Assets.at("stylesheets/css/style-mobile.css")', containers: '100%!', grid: { collapse: true, gutters: 20 }, viewport: { scalable: false } }
 		},
 		plugins: {
 			layers: {
