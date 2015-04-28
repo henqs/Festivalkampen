f371ee5ccd26e588eec21ef53f84a303190c812b
henqs <henq.sv@gmail.com>
2015-04-28 11:10:05 +0200

    Något

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -25,7 +25,17 @@ public class Application extends Controller {
 	    return ok(index.render());
 	  }
 	  
-	
+//	  public static Result javascriptRoutes() {
+//		    response().setContentType("text/javascript");
+//		    return ok(
+//		        Routes.javascriptRouter("jsRoutes",
+//		            controllers.routes.javascript.Projects.add(),
+//		            controllers.routes.javascript.Projects.delete(),
+//		            controllers.routes.javascript.Projects.rename(),
+//		            controllers.routes.javascript.Projects.addGroup()
+//		        )
+//		    );
+//		}
 	
 //	// Create user and send to database
 		public static void addUser() {
