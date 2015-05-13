0165f4efc42bfa7a4feed57fa513b7ae9a5f42a9
svett89 <axel.naucler@gmail.com>
2015-05-13 15:11:57 +0200

    Fungerande QR-kodskanning på desktop
    Den här versionen har en QR-skanner på spelhub-sidan (tryck på Spel för
    att komma till den) som funkar! Just nu är den bara kodad så att om den
    hittar en qr-kod så försöker den redirecta användaren till sidan som
    QR-koden representerar. Den här funktionaliteten är enkel att ändra på
    dock.
    
    Det stora problemet är att få den här skiten att fungera på mobil. Om
    man kör den här versionen på mobil (i alla fall Android, vore bra att
    testa med en iPhone också) så tar den bara en stillbild med kameran på
    framsidan av telefonen istället för att visa en videoström och skanna
    kontinuerligt.

---
+++ public/javascripts/mainqrcode.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#reader').html5_qrcode(function(data){
+			$('#read').html(data);
+			window.open(data);
+		},
+		function(error){
+			$('#read_error').html(error);
+		}, function(videoError){
+			$('#vid_error').html(videoError);
+		}
+	);
+});
